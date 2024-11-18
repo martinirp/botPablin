44794fb8c784584f33a3e6ba15aa322975ba6bf0
Lucas Rezende <96215982+martinirp@users.noreply.github.com>
2024-11-18 16:36:36 -0300

    Update get_cookies.py

--- get_cookies.py
+++ get_cookies.py
@@ -1,10 +1,16 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 import time
 import pickle
 
+# Configuração do caminho do Chromium (via Snap)
+chrome_options = Options()
+chrome_options.binary_location = "/snap/bin/chromium"  # Caminho correto do Chromium no Snap
+
 # Usar o webdriver-manager para baixar e configurar o chromedriver automaticamente
-driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
 # Acessar o YouTube
 driver.get("https://www.youtube.com")
